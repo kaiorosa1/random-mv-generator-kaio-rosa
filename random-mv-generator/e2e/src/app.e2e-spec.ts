ada84bc26cdca392ca795fdf04d2045552a50f1a
Kaio Rosa <kaio.rosa@motora.ai>
2020-05-03 17:02:28 -0300

    rename project

--- random-mv-generator/e2e/src/app.e2e-spec.ts
+++ random-mv-generator/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('random-meal-generator app is running!');
+    expect(page.getTitleText()).toEqual('random-mv-generator app is running!');
   });
 
   afterEach(async () => {
