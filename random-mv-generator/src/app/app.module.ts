c9e3ef4e9a80b5e96de45764ed69ae7655e59cf5
Kaio Rosa <kaio.rosa@motora.ai>
2020-05-03 19:44:46 -0300

    add mvService updated

--- random-mv-generator/src/app/app.module.ts
+++ random-mv-generator/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { MvpageComponent } from './mvpage/mvpage.component';
+import { SafePipe } from './pipes/safe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
-    MvpageComponent
+    MvpageComponent,
+    SafePipe
   ],
   imports: [
     BrowserModule,
--- random-mv-generator/src/app/models/mvpage.ts
+++ random-mv-generator/src/app/models/mvpage.ts
@@ -1,6 +1,7 @@
 export interface MvPage {
     artist: String;
     titletrack: String;
+    imgurl: String;
     videourl: String;
     lyrics?: Object
 }
